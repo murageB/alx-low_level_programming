9667ed3bf469a2435a026528b9fcc5ed17b8e6b5
murageB <brendanrichard96@gmail.com>
2021-02-05 17:53:05 +0000

    Added new file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+  char alph;
+
+  for(alph = 'a' ; alph <= 'z' ; alph++)
+    {
+      putchar(alph);
+    }
+  for(alph = 'A' ; alph <= 'Z' ; alph++)
+    {
+      putchar(alph);
+    }
+
+  putchar('\n');
+
+  return(0);
+}
