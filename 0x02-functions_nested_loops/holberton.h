a59ededf9521415a76b1c90ebacd70de66cd5a17
murageB <brendanrichard96@gmail.com>
2021-02-10 11:47:45 +0000

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/* THis file all prototypes used in project 0x02. C-Functions,nested loopsn*/
+/* THis file all prototypes used in project 0x02. C-Functions,nested loops*/
 
 /**
  * _putchar - putchar
@@ -13,7 +13,13 @@ int _putchar(char c);
  */
 void print_alphabet(void);
 
-
-
+/**
+ * print p10x the alphabet in lowercase
+ */
+void print_alphabet_x10(void);
+/**
+ *checks for lower character.
+ */
+int _islower(int c);
 
 #endif
