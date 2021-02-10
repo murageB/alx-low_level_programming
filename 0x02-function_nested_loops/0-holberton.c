4fe7e051602eddb34a6ff6368335ff805d094c38
murageB <brendanrichard96@gmail.com>
2021-02-10 07:55:50 +0000

    Added new file 0-holberton.c

--- 0x02-function_nested_loops/0-holberton.c
+++ 0x02-function_nested_loops/0-holberton.c
@@ -6,14 +6,13 @@
 
 int main(void)
 {
-  int h = 0;
-  char sch[9] = "Holberton";
-  
-  while (h < 9)
-    {
-      _putchar(sch[h]);
-      h += 1;
-    }
-  _putchar('\n');
-  return (0);
+int h = 0;
+char sch[9] = "Holberton";
+while (h < 9)
+{
+_putchar(sch[h]);
+h += 1;
+}
+_putchar('\n');
+return (0);
 }
