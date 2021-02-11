562a119feafef1982c662f027eca4d19aa39cc06
murageB <brendanrichard96@gmail.com>
2021-02-11 19:43:30 +0000

    Updated holberton.h

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,31 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *_putchar
+ */
+int _putchar(char c);
+
+/**
+ *checks for uppercase.
+ */
+int _isupper(int c);
+
+/**
+ *checks for a digit(0 through 9).
+ */
+int _isdigit(int c);
+
+/**
+ *multiplies two integers.
+ */
+int mul(int a, int b);
+
+/**
+*print the numbers, from 0 to 9.
+*/
+void print_numbers(void); 
+
+
+
+#endif
