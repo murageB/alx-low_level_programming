29bb4f1c6051499928d0c0317aad98ebb979066b
murageB <brendanrichard96@gmail.com>
2021-02-16 18:12:32 +0000

    Added new file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - changes the value to integer using pointer
+ *@n :pointer to the interger.
+ *Return: nothing.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
