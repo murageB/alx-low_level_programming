4e16ece509333c7c4f2e13770e0b7bdbef3fc120
murageB <brendanrichard96@gmail.com>
2021-02-18 21:05:35 +0000

    Updated holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -24,8 +24,7 @@ int _strcmp(char *s1, char *s2);
 
 /**
  *reverse_array - reverses the content of an array of integers
- *@n: number of of elements to swap.
  */
-void reverse_array(int *a, int n)
+void reverse_array(int *a, int n);
 
 #endif
