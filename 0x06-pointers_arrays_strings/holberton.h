e93f5052c8ae50943af7c9fefdd1612f9632d385
murageB <brendanrichard96@gmail.com>
2021-02-18 20:14:48 +0000

    Updated holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -17,6 +17,11 @@ char *_strncat(char *dest, char *src, int n);
  */
 char *_strncpy(char *dest, char *src, int n);
 
+/**
+ *_strcmp - compares two strings
+ */
+int _strcmp(char *s1, char *s2);
+
 
 
 #endif
