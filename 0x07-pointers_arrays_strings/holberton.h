169df98e9cc18bbc7baea5e74a0b5f8a5cb11d98
murageB <brendanrichard96@gmail.com>
2021-02-22 07:49:43 +0000

    Added new file holberton.h

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *_memset - fills memory with constant byte.
+ */
+char *_memset(char *s, char *b, unsigned int n);
+
+/**
+ *_memcpy - copies memory area.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
+#endif
