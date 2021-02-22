6fa8ffb406b8d6825b309db21b718692b44c6294
murageB <brendanrichard96@gmail.com>
2021-02-22 17:35:00 +0000

    Added 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *_strchr-locates a character in a string
+ *@s: pointer to the first occurrence of the character c 
+ *@c: character 
+ *
+ *Return: s if the character if found or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s != c && *s != '\0')
+{
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+else 
+{
+return ("");
+}
+}
+ 
