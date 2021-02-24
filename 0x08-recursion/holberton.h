4905f6e34b7e63d84a68c581a0236e44aeb0f1a6
murageB <brendanrichard96@gmail.com>
2021-02-24 11:47:43 +0000

    Updated holberton.h

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *_putchar-writes the character c to stdout
+ */
+int _putchar(char c);
+
+/**
+ *_puts_recursion-prints a string
+ */
+void _puts_recursion(char *s);
+
+/**
+ *_print_rev_recursion-prints string in reverse
+ */
+void _print_rev_recursion(char *s);
+
+/**
+ *_strlen_recursion-returns length of a string
+ */
+int _strlen_recursion(char *s);
+
+
+#endif
