0ba398b26cc78354b0c9a44154192a9059376ffc
murageB <brendanrichard96@gmail.com>
2021-02-24 15:40:18 +0000

    Added new file 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,13 @@
 
 void _puts_recursion(char *s)
 {
-int i = 0;
-
-if (*(s + i) == '\0')
+if (*s == '\0')
+{
+_putchar('\n');
 return;
+}
 else
-_putchar(s[i]);
+_putchar(*s);
 _puts_recursion(s + 1);
 }
 
