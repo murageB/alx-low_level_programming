6afed6f62d2f0ec0d04afdc71053adf13216b329
murageB <brendanrichard96@gmail.com>
2021-02-24 21:20:06 +0000

    Updated holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -31,5 +31,19 @@ int factorial(int n);
  */
 int _pow_recursion(int x, int y);
 
+/**
+ *_sqrt_recursion-finds the natural square root of a number
+ */
+int _sqrt_recursion(int n);
+
+/**
+ *checks for prime numbers
+ */
+int is_prime_number(int n);
+
+/**
+ *is_palindrome-checks is string is a palindrome
+ */
+int is_palindrome(char *s);
 
 #endif
