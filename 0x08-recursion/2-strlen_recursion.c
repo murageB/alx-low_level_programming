bc7586c9e6715bda00f944293562d917dab813cd
murageB <brendanrichard96@gmail.com>
2021-02-24 12:40:53 +0000

    Added new file 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_strlen_recursion-finds the length of a string
+ *@s: string to be checked
+ *
+ *Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return 1 + _strlen_recursion(s + 1);
+}
