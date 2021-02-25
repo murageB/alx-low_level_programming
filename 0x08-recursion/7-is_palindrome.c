7b8b23f09b37f61cffaf27d93f8f8d1f24127729
murageB <brendanrichard96@gmail.com>
2021-02-25 09:56:02 +0000

    Added new file 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -5,27 +5,24 @@
  *@s: pointer to the string to be checked
  *@a: first integer in string
  *@b: last integer in string
- *@flag: holding address of return value
- *Return:void
+ *
+ *Return: Return:1 if a string is a palindrome and 0 if not
  */
-void pali_check(char *s, int a, int b, int *flag)
+int pali_check(char *s, int a, int b)
 {
-if (a < b + 1)
-{
-if (s[a] != s[b])
-{
-flag *= 0;
-}
-else
-{
-flag *= 1;
-}
-pali_check(s, a + 1, b - 1, flag);
+if (a == b)
+return (1);
+else if (s[a] != s[b])
+return (0);
+if (a < b)
+return (pali_check(s, a + 1, b - 1));
+return (1);
 }
 
 /**
  *_strlen_recursion-finds the length of a string
  *@s: string to be checked
+ *
  *Return: length of string
  */
 int _strlen_recursion(char *s)
@@ -39,13 +36,12 @@ return (1 + _strlen_recursion(s + 1));
 /**
  *is_palindrome-checks if string is a palindrome
  *@s: pointer to string to be checked
+ *
  *Return:1 if a string is a palindrome and 0 if not
  */
 int is_palindrome(char *s)
 {
-int flag = 1;
-if (_strlen_recursion(s) == 0)
-return (flag);
-pali_check(s, 0, _strlen_recursion(s) - 1, &flag);
-return (flag);
+if (!*s)
+return (1);
+return (pali_check(s, 0, _strlen_recursion(s) - 1));
 }
