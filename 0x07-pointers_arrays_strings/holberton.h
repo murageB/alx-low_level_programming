cf82fcb28df997716cfcee32f1329e1a7445504a
murageB <brendanrichard96@gmail.com>
2021-02-25 07:33:30 +0000

    Updated holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -2,6 +2,11 @@
 #define HOLBERTON_H
 
 /**
+ *_putchar-writes the character c to stdout
+ */
+int _putchar(char c);
+
+/**
  *_memset - fills memory with constant byte.
  */
 char *_memset(char *s, char b, unsigned int n);
