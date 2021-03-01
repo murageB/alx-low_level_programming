ca3b8e374e03b9ff3e98951719942c1b558de01f
murageB <brendanrichard96@gmail.com>
2021-03-01 12:51:21 +0000

    Added new file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main-prints its name
+ *@argc: integer
+ *@argv: pointer to a string
+ *
+ *Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
