9b3c8e83e3481cefcad79bccc6d4eba188aae931
murageB <brendanrichard96@gmail.com>
2021-03-02 14:53:06 +0000

    Updated holberton.h

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,10 +1,20 @@
-#ifndef
-#define
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 /**
  *_putchar-putchar
  */
 int _putchar(char c);
+
+/**
+ *create_array-creates an array of char, and initializes it with a specific char.
+ */
+char *create_array(unsigned int size, char c);
+
+/**
+ *_strdup-returns an allocatted space in mem, which contains a duplicate string
+ */
+char *_strdup(char *str);
 
 
 
