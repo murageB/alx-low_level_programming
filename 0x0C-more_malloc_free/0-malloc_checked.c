6a9256fe4b2d431481fb6991df6124ae95802204
murageB <brendanrichard96@gmail.com>
2021-03-04 11:59:25 +0000

    README.md

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *malloc_checked - allocates memory using malloc
+ *@b: size of memory allocated
+ *
+ *Return: a pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *mem = malloc(b);
+
+if (!mem)
+exit (98);
+else
+return (mem);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+## 0x0C. C - More malloc, free
+This folder contains the files of the tasks of Project [0x0C. C - More malloc, free ](https://alx-intranet.hbtn.io/projects/223)
