3fd9d8f137cf6cf5f57cb623ab0fb0c95122dc01
murageB <brendanrichard96@gmail.com>
2021-03-08 17:01:39 +0000

    created new type to store information about the dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog- stored information about the dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: ownwer of the dog
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
+#endif
