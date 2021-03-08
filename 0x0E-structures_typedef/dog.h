c6f6ce51643f09fd4d479365e981e20825b8c543
murageB <brendanrichard96@gmail.com>
2021-03-08 19:39:51 +0000

    added typedef dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,11 @@ float age;
 char *owner;
 };
 
+/**
+ *dog_t - typedef for dog
+ */
+typedef struct dog dog_t; 
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
