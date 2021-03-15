c27929635f9c7b6547e08cc06147e85469e6f8c4
murageB <brendanrichard96@gmail.com>
2021-03-15 11:28:56 +0000

    operator functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,37 +3,63 @@
 #include <stdlib.h>
 
 
+/**
+ *op_add - adds two integers
+ *@a: integer
+ *@b: integer
+ *Return: returns the sum of a and b
+ */
 int op_add(int a, int b)
 {
-  return (a + b);
+return (a + b);
 }
-
+/**
+ *op_sub - subtracts two integer
+ *@a: int
+ *@b: int
+ *Return: returns the difference of a and b
+ */
 int op_sub(int a, int b)
 {
-  return (a - b);
+return (a - b);
 }
-
+/**
+ *op_mul - multiplies two integers
+ *@a: int
+ *@b: int
+ *Return: returns the product of a and b
+ */
 int op_mul(int a, int b)
 {
-  return (a * b);
+return (a * b);
 }
-
-int op_div(int a , int b)
+/**
+ *op_div - divides two integers
+ *@a: int
+ *@b: int
+ *Return: returns the result of the division of a by b
+ */
+int op_div(int a, int b)
 {
-  if (b == 0)
-    {
-      printf("Error\n");
-      exit(100);
-    }
-  return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
 }
-
+return (a / b);
+}
+/**
+ *op_mod- finds the reminder of the division of two intergers
+ *@a: int
+ *@b: int
+ *Return: returns the remainder of the division of a by b
+ */
 int op_mod(int a, int b)
 {
-  if (b == 0)
-    {
-      printf("Error\n");
-      exit(100);
-    }
-  return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
