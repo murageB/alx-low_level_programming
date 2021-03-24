3d8fe6b00eae996298a115b00d9b4432359e5b9a
murageB <brendanrichard96@gmail.com>
2021-03-24 11:29:30 +0000

    frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * free_list - frees a list_t list.
+ * @head: pointer to first node
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+size_t nodes = 0;
+
+if (h != NULL)
+return (0);
+while (h != NULL)
+{
+nodes++;
+h = h->next;
+}
+return (nodes);
+}
