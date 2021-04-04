f18826ce061c19087e46f85d7529dec4488b8b7b
murageB <brendanrichard96@gmail.com>
2021-04-04 20:16:14 +0000

    remove free()

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,18 +12,22 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *new_node;
+listint_t *current = NULL;
+listint_t *new_node = NULL;
 
+current = *head;
 new_node = malloc(sizeof(listint_t));
 
 if (!new_node)
 return (NULL);
 
 new_node->n = n;
-new_node->next = NULL;
-new_node->next = *head;
-*head = new_node;
+(*head) = new_node;
 
-free(new_node);
+if(new_node == NULL)
+new_node->next = NULL;
+else
+new_node->next = current;
+
 return (new_node);
 }
